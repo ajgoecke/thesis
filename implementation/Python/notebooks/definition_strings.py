69cd40437e7fa76ae8ea3110e0964a536ad8ad39
Anna Goecke <59655309+ajgoecke@users.noreply.github.com>
2022-10-10 11:28:05 +0200

    worked on definition phrasing

---
+++ implementation/Python/notebooks/definition_strings.py
@@ -0,0 +1,20 @@
+str_base_pers =  """Der Begriff {COMPOUND} bezeichnet eine/n {DEFINITION} im Bezug auf den Klimawandel.\nDer Begriff wird in unserem Korpus {CON_FREQ} Mal von den Klimaskeptikern und {PRO_FREQ} Mal von den Klimaaktivisten verwendet und ist {SENTIMENT} konnotiert. """
+
+str_base_loc = """Der Begriff {COMPOUND} bezeichnet eine/n {DEFINITION} im Bezug auf den Klimawandel.\nDer Begriff wird in unserem Korpus {CON_FREQ} Mal von den Klimaskeptikern und {PRO_FREQ} Mal von den Klimaaktivisten verwendet und ist {SENTIMENT} konnotiert. """
+
+str_base_action = """{COMPOUND} bezeichnet eine/n {DEFINITION} im Bezug auf den Klimawandel.\nDer Begriff wird in unserem Korpus {CON_FREQ} Mal von den Klimaskeptikern und {PRO_FREQ} Mal von den Klimaaktivisten verwendet und ist {SENTIMENT} konnotiert. """
+
+str_base_abstract = """Ein {COMPOUND} ist {DEFINITION} im Bezug auf den Klimawandel.\nDer Begriff wird in unserem Korpus {CON_FREQ} Mal von den Klimaskeptikern und {PRO_FREQ} Mal von den Klimaaktivisten verwendet und ist {SENTIMENT} konnotiert. """
+str_base_group = """Als {COMPOUND} wird eine Personengruppe bezeichnet, die {DEFINITION} im Bezug auf den Klimawandel.\nDer Begriff wird in unserem Korpus {CON_FREQ} Mal von den Klimaskeptikern und {PRO_FREQ} Mal von den Klimaaktivisten verwendet und ist {SENTIMENT} konnotiert. """
+
+
+# INITIATE STRINGS THAT ARE EQUAL FOR ALL CATEGORIES
+
+str_pers_con = """\nPersonen, die im Zusammenhang mit dem Begriff erwähnt werden sind {CON_PERS} (Con Corpus) """
+str_pers_pro = """und {PRO_PERS} (Pro Corpus). """
+str_org_con = """\nAußerdem werden im Kontext von {COMPOUND} folgende Organisationen genannt: {CON_ORG} (C2022)"""
+str_org_pro = """und {PRO_ORG} (P2022). """    
+str_simwords = """\nSiehe auch: {SIMILAR_WORDS}"""
+
+
+str_test = "YYY"
